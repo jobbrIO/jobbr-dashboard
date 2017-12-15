a025fa67198167a3d8e165b6d9ec665f60c84ce3
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-12-15 17:00:26 +0100

    wip checkboxes

--- source/Jobbr.Dashboard.Frontend/jobbr-dashboard/src/main.ts
+++ source/Jobbr.Dashboard.Frontend/jobbr-dashboard/src/main.ts
@@ -13,6 +13,7 @@ import '../static/styles/layout.scss';
 import '../static/styles/text.scss';
 import '../static/styles/box.scss';
 import '../static/styles/list.scss';
+import '../static/styles/checkbox.scss';
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
