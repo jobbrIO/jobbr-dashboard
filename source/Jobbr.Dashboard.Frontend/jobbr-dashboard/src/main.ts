31d39f37f2aac7b402b46db25be022860735144e
Oliver Zürcher <oliver.zuercher@gmail.com>
2018-10-08 16:03:17 +0200

    updated npm packages

--- source/Jobbr.Dashboard.Frontend/jobbr-dashboard/src/main.ts
+++ source/Jobbr.Dashboard.Frontend/jobbr-dashboard/src/main.ts
@@ -1,22 +1,27 @@
 /// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
 // we want font-awesome to load as soon as possible to show the fa-spinner
+
+import * as Bluebird from 'bluebird';
+Bluebird.config({ warnings: { wForgottenReturn: false }, longStackTraces: false });
+
 import 'whatwg-fetch';
 import 'element-closest';
 import {Aurelia} from 'aurelia-framework';
 import environment from './environment';
 import {PLATFORM} from 'aurelia-pal';
-import * as Bluebird from 'bluebird';
+
 import 'jquery';
 import 'popper.js';
 
-import '../node_modules/bootstrap/js/src/button';
-import '../node_modules/bootstrap/js/src/dropdown';
-import '../node_modules/bootstrap/js/src/collapse';
-import '../node_modules/bootstrap/js/src/popover';
+import 'bootstrap';
+// import '../node_modules/bootstrap/js/src/button';
+// import '../node_modules/bootstrap/js/src/dropdown';
+// import '../node_modules/bootstrap/js/src/collapse';
+// import '../node_modules/bootstrap/js/src/popover';
 
 import '../static/styles/styles.scss';
 
-Bluebird.config({ warnings: { wForgottenReturn: false }, longStackTraces: false });
+
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
