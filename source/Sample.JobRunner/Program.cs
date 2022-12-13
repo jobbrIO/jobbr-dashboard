af3e1ec73907ae35f66e7d0eb2a36bba8bf00cde
Roope Kivioja <roope.kivioja@zuehlke.com>
2022-12-13 13:00:41 +0100

    Upgrade Jobbr.* NuGet packages to .NET 6 RC versions

--- source/Sample.JobRunner/Program.cs
+++ source/Sample.JobRunner/Program.cs
@@ -9,10 +9,10 @@ namespace Sample.JobRunner
         public static void Main(string[] args)
         {
             // Set the default assembly to query for job types
-            var runtime = new ForkedRuntime(new RuntimeConfiguration { JobTypeSearchAssemblies = new [] { typeof(MinutelyJob).Assembly } });
+            //var runtime = new ForkedRuntime(new RuntimeConfiguration { JobTypeSearchAssemblies = new [] { typeof(MinutelyJob).Assembly } });
 
-            // Pass the arguments of the forked execution to the runtime
-            runtime.Run(args);
+            //// Pass the arguments of the forked execution to the runtime
+            //runtime.Run(args);
         }
     }
 }
