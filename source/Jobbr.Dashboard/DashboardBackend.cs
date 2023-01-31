1a1c530e512cdf97948fef6dfaec6e704e8db2b2
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2023-01-31 10:17:37 +0100

    Switch to using SharpFileSystem
    The previous solution relied on unpacking the archive in the temp
    directory, which breaks the self-contained constraint and could
    potentially cause issues on systems without write access

--- source/Jobbr.Dashboard/DashboardBackend.cs
+++ source/Jobbr.Dashboard/DashboardBackend.cs
@@ -5,132 +5,133 @@ using Jobbr.Dashboard.Controller;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using SimpleInjector;
 
-namespace Jobbr.Dashboard
+namespace Jobbr.Dashboard;
+
+/// <summary>
+///     The dashboard server backend.
+/// </summary>
+public class DashboardBackend : IJobbrComponent
 {
+    private readonly DashboardConfiguration _configuration;
+    private readonly ILogger _logger;
+    private readonly InstanceProducer[] _serviceContainer;
+
+    private static IFileProvider _virtualFileSystemProvider;
+    private IWebHost _webHost;
+
     /// <summary>
-    /// The dashboard server backend.
+    ///     Initializes a new instance of the <see cref="DashboardBackend" /> class.
     /// </summary>
-    public class DashboardBackend : IJobbrComponent
+    public DashboardBackend(ILoggerFactory loggerFactory, Container serviceContainer, DashboardConfiguration configuration)
     {
-        private readonly ILogger _logger;
-        private readonly InstanceProducer[] _serviceContainer;
-        private readonly DashboardConfiguration _configuration;
+        _logger = loggerFactory.CreateLogger<DashboardBackend>();
+        _serviceContainer = serviceContainer.GetCurrentRegistrations();
+        _configuration = configuration;
 
-        private IWebHost _webHost;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DashboardBackend"/> class.
-        /// </summary>
-        public DashboardBackend(ILoggerFactory loggerFactory, Container serviceContainer, DashboardConfiguration configuration)
+        if (_virtualFileSystemProvider == null)
         {
-            _logger = loggerFactory.CreateLogger<DashboardBackend>();
-            _serviceContainer = serviceContainer.GetCurrentRegistrations();
-            _configuration = configuration;
+            _virtualFileSystemProvider = new ZipFileContentProvider("dashboard-app.zip");
+        }
+    }
+
+    /// <summary>
+    ///     Start the component.
+    /// </summary>
+    public void Start()
+    {
+        if (_webHost != null)
+        {
+            throw new InvalidOperationException("The server has already been started.");
         }
 
-        /// <summary>
-        /// Start the component.
-        /// </summary>
-        public void Start()
+        if (string.IsNullOrWhiteSpace(_configuration.BackendAddress))
         {
-            if (_webHost != null)
+            throw new ArgumentException("Unable to start DashboardBackend when no backend URL is specified.", nameof(_configuration.BackendAddress));
+        }
+
+        _webHost = new WebHostBuilder()
+            .UseKestrel()
+            .UseUrls(new Uri(_configuration.BackendAddress).GetLeftPart(UriPartial.Authority))
+            .ConfigureServices(services =>
             {
-                throw new InvalidOperationException("The server has already been started.");
-            }
+                foreach (var instanceProducer in _serviceContainer)
+                {
+                    services.Add(new ServiceDescriptor(instanceProducer.ServiceType, instanceProducer.GetInstance()));
+                }
 
-            if (string.IsNullOrWhiteSpace(_configuration.BackendAddress))
+                // Controllers with endpoints need to be added manually due to discovery issues.
+                // https://stackoverflow.com/q/73777145
+                services.AddControllers()
+                    .AddApplicationPart(typeof(ConfigController).Assembly)
+                    .AddApplicationPart(typeof(CronController).Assembly)
+                    .AddApplicationPart(typeof(SystemController).Assembly);
+            })
+            .Configure(app =>
             {
-                throw new ArgumentException("Unable to start DashboardBackend when no backend URL is specified.", nameof(_configuration.BackendAddress));
-            }
+                app.UseRouting();
 
-            _webHost = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls(new Uri(_configuration.BackendAddress).GetLeftPart(UriPartial.Authority))
-                .ConfigureServices(services =>
+                app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+                app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+                app.UseFileServer(new FileServerOptions
                 {
-                    foreach (var instanceProducer in _serviceContainer)
+                    EnableDefaultFiles = true,
+                    FileProvider = _virtualFileSystemProvider,
+                    StaticFileOptions =
                     {
-                        services.Add(
-                            new ServiceDescriptor(instanceProducer.ServiceType, instanceProducer.GetInstance()));
-                    }
-
-                    // Controllers with endpoints need to be added manually due to discovery issues.
-                    // https://stackoverflow.com/q/73777145
-                    services.AddControllers()
-                        .AddApplicationPart(typeof(ConfigController).Assembly)
-                        .AddApplicationPart(typeof(CronController).Assembly)
-                        .AddApplicationPart(typeof(SystemController).Assembly);
-                })
-                .Configure(app =>
-                {
-                    app.UseFileServer(new FileServerOptions
-                    {
-                        EnableDefaultFiles = true,
-                        FileProvider = new ZipContentTypeProvider(),
-                        DefaultFilesOptions =
-                        {
-                            DefaultFileNames = new[]
-                            {
-                                "index.html"
-                            }
-                        }
-                    });
-
-                    app.UseStaticFiles(new StaticFileOptions
-                    {
-                        FileProvider = new ZipContentTypeProvider(),
+                        FileProvider = _virtualFileSystemProvider,
                         ServeUnknownFileTypes = true
-                    });
-
-                    app.UseRouting();
-
-                    app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
-                    app.UseEndpoints(endpoints =>
+                    },
+                    DefaultFilesOptions =
                     {
-                        endpoints.MapControllers();
-                    });
-                })
-                .Build();
+                        DefaultFileNames = new[]
+                        {
+                            "index.html"
+                        }
+                    }
+                });
+            })
+            .Build();
 
-            _webHost.Start();
+        _webHost.Start();
 
-            _logger.LogInformation("Started web host for DashboardBackend at '{backendAddress}'", _configuration.BackendAddress);
-        }
+        _logger.LogInformation("Started web host for DashboardBackend at '{backendAddress}'", _configuration.BackendAddress);
+    }
 
-        /// <summary>
-        /// Stop web host.
-        /// </summary>
-        public void Stop()
+    /// <summary>
+    ///     Stop web host.
+    /// </summary>
+    public void Stop()
+    {
+        _logger.LogInformation("Stopping web host for Web-Endpoints");
+
+        Task.FromResult(_webHost.StopAsync());
+    }
+
+    /// <summary>
+    ///     Dispose web host.
+    /// </summary>
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        Dispose(true);
+    }
+
+    /// <summary>
+    ///     Conditional web host dispose.
+    /// </summary>
+    /// <param name="disposing">If true, dispose.</param>
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
         {
-            _logger.LogInformation("Stopping web host for Web-Endpoints");
-
-            Task.FromResult(_webHost.StopAsync());
-        }
-
-        /// <summary>
-        /// Dispose web host.
-        /// </summary>
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-            Dispose(true);
-        }
-
-        /// <summary>
-        /// Conditional web host dispose.
-        /// </summary>
-        /// <param name="disposing">If true, dispose.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                Task.Run(async () => await _webHost.StopAsync());
-                _webHost?.Dispose();
-            }
+            Task.Run(async () => await _webHost.StopAsync());
+            _webHost?.Dispose();
         }
     }
 }
